689be802408c6abbbdfc78b620f51d4afb0a3843
2022040596-Zeng Guang Sen <1270653169@qq.com>
2022-07-29 20:36:53 +0800

    task 0.7.10.3 before使得代码支持soc和非soc的切换

--- projects/chisel_cpu_diff/src/main/scala/SimTop.scala
+++ projects/chisel_cpu_diff/src/main/scala/SimTop.scala
@@ -3,43 +3,8 @@ import chisel3._
 import chisel3.util._
 // import difftest._
 
-class SocAXI4IO extends Bundle{
-  val awready = Input(Bool())
-  val awvalid = Output(Bool())
-  val awaddr  = Output(UInt(32.W))
-  val awid    = Output(UInt(4.W))
-  val awlen   = Output(UInt(8.W))
-  val awsize  = Output(UInt(3.W))
-  val awburst = Output(UInt(2.W))
-  
-  val wready  = Input(Bool())
-  val wvalid  = Output(Bool())
-  val wdata   = Output(UInt(64.W))
-  val wstrb   = Output(UInt(8.W))
-  val wlast   = Output(Bool())
-  
-  val bready  = Output(Bool())
-  val bvalid  = Input(Bool())
-  val bresp   = Input(UInt(2.W))
-  val bid     = Input(UInt(4.W))
 
-  val arready = Input(Bool())
-  val arvalid = Output(Bool())
-  val araddr  = Output(UInt(32.W))
-  val arid    = Output(UInt(4.W))
-  val arlen   = Output(UInt(8.W))
-  val arsize  = Output(UInt(3.W))
-  val arburst = Output(UInt(2.W))
-
-  val rready  = Output(Bool())
-  val rvalid  = Input(Bool())
-  val rresp   = Input(UInt(2.W))
-  val rdata   = Input(UInt(64.W))
-  val rlast   = Input(Bool())
-  val rid     = Input(UInt(4.W))
-}
-
-class SimTop extends Module {
+class SimTop extends Module{
   val io = IO(new Bundle {
     // val logCtrl = new LogCtrlIO
     // val perfInfo = new PerfInfoIO
